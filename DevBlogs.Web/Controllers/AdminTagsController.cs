1febdf4fc9b3f41441a7d3d221279714ffbb7df6
Pronit21 <pronitparida2000@gmail.com>
2024-08-03 22:36:57 +0530

    Add project files.

---
+++ DevBlogs.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using DevBlogs.Web.Data;
+using DevBlogs.Web.Models.Domain;
+using DevBlogs.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DevBlogs.Web.Controllers
+{
+	public class AdminTagsController : Controller
+	{
+		private readonly DevBlogDbContext _devBlogDbContext;
+
+		public AdminTagsController(DevBlogDbContext devBlogDbContext)
+		{
+			_devBlogDbContext = devBlogDbContext;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ActionName("Add")]
+		public IActionResult SubmitTag(AddTagRequest addTagRequest)
+		{
+			//Mapping AddTagRequest to Tag domain model
+			var tag = new Tag
+			{
+				Name = addTagRequest.Name,
+				DisplayName = addTagRequest.DisplayName
+			};
+
+			_devBlogDbContext.Add(tag);
+			_devBlogDbContext.SaveChanges();
+
+			return RedirectToAction("ShowList");
+		}
+
+		[HttpGet]
+		[ActionName("ShowList")]
+		public IActionResult ShowList()
+		{
+			var tags = _devBlogDbContext.Tags.ToList();
+			return View(tags);
+		}
+
+		[HttpGet]
+		public IActionResult Edit(Guid id)
+		{
+			var tag = _devBlogDbContext.Tags.FirstOrDefault(t => t.Id == id);
+			return View();
+		}
+	}
+}
---
+++ DevBlogs.Web/Data/DevBlogDbContext.cs
@@ -0,0 +1,27 @@
+﻿using DevBlogs.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DevBlogs.Web.Data
+{
+    public class DevBlogDbContext : DbContext
+    {
+        public DevBlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public class BloggingContextFactory : IDesignTimeDbContextFactory<DevBlogDbContext>
+        {
+            public DevBlogDbContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<DevBlogDbContext>();
+                optionsBuilder.UseSqlServer("Server=DESKTOP-J493CPQ\\SQLEXPRESS; Database=DevBlogDB; Trusted_Connection=True; MultipleActiveResultSets=true; TrustServerCertificate=True");
+
+                return new DevBlogDbContext(optionsBuilder.Options);
+            }
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
